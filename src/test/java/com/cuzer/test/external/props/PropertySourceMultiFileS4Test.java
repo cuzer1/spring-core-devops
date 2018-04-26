c79f4e89438cbf36212fbf25d3ccd5390442e28b
CUzer <cengizuzer@hotmail.com>
2018-04-26 00:39:41 -0700

    Added multiple properties

--- src/test/java/com/cuzer/test/external/props/PropertySourceMultiFileS4Test.java
+++ src/test/java/com/cuzer/test/external/props/PropertySourceMultiFileS4Test.java
@@ -9,11 +9,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.cuzer.test.jms.FakeJmsBroker;
-import com.test.config.external.props.ExternalPropsEnv;
+import com.test.config.external.props.ExternalPropsMultiFileS4;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = ExternalPropsEnv.class)
-public class PropertySourceEnvTest {
+@ContextConfiguration(classes = ExternalPropsMultiFileS4.class)
+public class PropertySourceMultiFileS4Test {
 
 	@Autowired
 	FakeJmsBroker fakeJmsBroker;
@@ -23,6 +23,6 @@ public class PropertySourceEnvTest {
 		assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
 		assertEquals(3330, fakeJmsBroker.getPort().intValue());
 		assertEquals("Ron", fakeJmsBroker.getUser());
-		assertEquals("Burgundy", fakeJmsBroker.getPassword());
+		assertEquals("Cokgizli", fakeJmsBroker.getPassword());
 	}
 }
--- src/test/java/com/cuzer/test/external/props/PropertySourceMultiFileTest.java
+++ src/test/java/com/cuzer/test/external/props/PropertySourceMultiFileTest.java
@@ -9,11 +9,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.cuzer.test.jms.FakeJmsBroker;
-import com.test.config.external.props.ExternalPropsEnv;
+import com.test.config.external.props.ExternalPropsMultiFile;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = ExternalPropsEnv.class)
-public class PropertySourceEnvTest {
+@ContextConfiguration(classes = ExternalPropsMultiFile.class)
+public class PropertySourceMultiFileTest {
 
 	@Autowired
 	FakeJmsBroker fakeJmsBroker;
@@ -23,6 +23,7 @@ public class PropertySourceEnvTest {
 		assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
 		assertEquals(3330, fakeJmsBroker.getPort().intValue());
 		assertEquals("Ron", fakeJmsBroker.getUser());
-		assertEquals("Burgundy", fakeJmsBroker.getPassword());
+		assertEquals("Cokgizli", fakeJmsBroker.getPassword());
 	}
+
 }
