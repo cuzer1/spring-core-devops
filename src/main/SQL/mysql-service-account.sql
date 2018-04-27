290695cad87b132a66d02fa5a8e89dbc0dec063c
CUzer <cengizuzer@hotmail.com>
2018-04-27 00:04:27 -0700

    Completed QA2 profile Assignment.

--- src/main/SQL/mysql-service-account.sql
+++ src/main/SQL/mysql-service-account.sql
@@ -14,4 +14,19 @@ GRANT insert on QA1.* to 'cuzer'@'%';
 GRANT update on QA1.* to 'cuzer'@'%';
 GRANT delete on QA1.* to 'cuzer'@'%';
 
+
+CREATE DATABASE QA2;
+
+CREATE USER 'cuzer'@'localhost' IDENTIFIED BY 'cingiz';
+GRANT select on QA2.* to 'cuzer'@'localhost';
+GRANT insert on QA2.* to 'cuzer'@'localhost';
+GRANT update on QA2.* to 'cuzer'@'localhost';
+GRANT delete on QA2.* to 'cuzer'@'localhost';
+
+CREATE USER 'cuzer'@'%' IDENTIFIED BY 'cingiz';
+GRANT select on QA2.* to 'cuzer'@'%';
+GRANT insert on QA2.* to 'cuzer'@'%';
+GRANT update on QA2.* to 'cuzer'@'%';
+GRANT delete on QA2.* to 'cuzer'@'%';
+
 FLUSH PRIVILEGES;
